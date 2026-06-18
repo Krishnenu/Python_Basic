6ee6231adc449158e92bb1f5666c70f28c6d2e9d
Krishnendu Narayan <57310676+Krishnenu@users.noreply.github.com>
2026-06-18 16:18:33 +0530

    problems added

---
+++ Problem_Solving/agegroup.py
@@ -0,0 +1,11 @@
+userage=int(input("Enter age:"))
+
+if  userage<13:
+    print("child")
+elif userage<20 & userage>12:
+    print("teenager")
+elif userage<60:
+    print("adult")
+else:
+    print("senior")
+
---
+++ Problem_Solving/grade.py
@@ -0,0 +1,21 @@
+score = 121
+
+if score>=101:
+    print("print verify your age")
+    exit()
+
+if score>=90:
+    grade="A"
+elif score>=80:
+    grade="B"
+elif score>=70:
+    grade="C"
+elif score>=60:
+    grade="D"
+elif score>=50:
+    grade="E"
+elif score>=40:
+    grade="F"
+
+
+print("grade",grade)
